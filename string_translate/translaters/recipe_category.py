067abdd30c214b326a53d6a4b9785c7e00f6bb6b
wilson <3050166231@qq.com>
2021-12-17 22:14:38 +0800

    add translate_recipe_category

--- string_translate/translaters/recipe_category.py
+++ string_translate/translaters/recipe_category.py
@@ -1,23 +1,22 @@
 from gettext import NullTranslations
 from ..helper import translate_any
-from .generic import translate_generic
 
 
 def translate_recipe_category(translation: NullTranslations, json_object):
-    pass
-# TODO this is id,if change miss track json
-# cid = json["id"]
-# if cid == 'CC_NONCRAFT':
-#     return
-# cat_name = cid.split("_")[1]
-# write_text(cat_name, origin, comment="Crafting recipes category name")
+    cid = json_object["id"]
+    if cid == 'CC_NONCRAFT':
+        return
+    cat_name = cid.split("_")[1]
+    json_object["name"] = translate_any(translation, cat_name)
 
-# for subcat in json.get("recipe_subcategories", []):
-#     if subcat == 'CSC_ALL':
-#         write_text("ALL", origin,
-#                    comment="Crafting recipes subcategory \"all\"")
-#     else:
-#         subcat_name = subcat.split('_')[2]
-#         write_text(subcat_name, origin,
-#                    comment="Crafting recipes subcategory of \"{}\" cat."
-#                    .format(cat_name))
+    for index, subcat in enumerate(json_object.get("recipe_subcategories", [])):
+        name = subcat
+        if subcat == 'CSC_ALL':
+            name = translate_any(translation, "ALL")
+        else:
+            subcat_name = subcat.split('_')[2]
+            name = translate_any(translation, subcat_name)
+        json_object["recipe_subcategories"][index] = {
+            "id": subcat,
+            "name": name
+        }
