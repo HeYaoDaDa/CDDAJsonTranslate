9e0f78113cf26119856b91bc25fd99983cdcd96d
HeYaoDaDa <37257891+HeYaoDaDa@users.noreply.github.com>
2022-02-09 21:50:31 +0800

    fix "烟熏架"

--- string_translate/helper.py
+++ string_translate/helper.py
@@ -1,14 +1,21 @@
 from gettext import NullTranslations
 
-
-def my_gettext(translation:NullTranslations, message:str, context:str = None):
+def my_gettext(translation:NullTranslations, context, single, plural=None):
     if context:
-        return translation.pgettext(context, message)
+        if not plural:
+            text = translation.pgettext(context, single)
+        if plural or text == single:
+            text = translation.npgettext(context, single, plural, 1)
     else:
-        return translation.gettext(message)
+        if not single:
+            return ''
+        if not plural:
+            text = translation.gettext(single)
+        if plural or text == single:
+            text = translation.ngettext(single, plural, 1)
+    return text
 
-
-def translate_any(translation:NullTranslations, message:str, context:str = None):
+def translate_any(translation:NullTranslations, message:any, context:str = None):
     if type(message) is list:
         for i in range(len(message)):
             message[i] = translate_any(translation, message[i], context)
@@ -17,16 +24,16 @@ def translate_any(translation:NullTranslations, message:str, context:str = None)
             context = message["ctxt"]
 
         if "str_pl" in message:
-            message["str_pl"] = my_gettext(translation, message["str_pl"], context)
+            message["str_pl"] = my_gettext(translation, context, message["str_pl"], message["str_pl"])
 
         if "str" in message:
-            message["str"] = my_gettext(translation, message["str"], context)
+            message["str"] = my_gettext(translation, context, message["str"])
         elif "str_sp" in message:
-            message["str_sp"] = my_gettext(translation, message["str_sp"], context)
+            message["str_sp"] = my_gettext(translation, context, message["str_sp"], message["str_sp"])
         else:
             raise Exception("ERROR: 'str' or 'str_sp' not found", message)
     elif type(message) is str:
-        message = my_gettext(translation, message, context)
+        message = my_gettext(translation, context, message)
     elif type(message) is int:
         pass
     elif message is None:
