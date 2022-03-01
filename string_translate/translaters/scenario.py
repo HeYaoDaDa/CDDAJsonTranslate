7042d7697aae340b262e28ba5f91e4c82822736c
HeYaoDaDa <37257891+HeYaoDaDa@users.noreply.github.com>
2022-03-01 22:54:01 +0800

    fix scenario.py bug

--- string_translate/translaters/scenario.py
+++ string_translate/translaters/scenario.py
@@ -3,29 +3,31 @@ from ..helper import translate_any
 
 
 def translate_scenario(translation: NullTranslations, json_object):
-    if type(json_object["name"]) is str:
-        scenario_name = json_object["name"]
-        json_object["name"] = {
-            "male": scenario_name, "female": scenario_name
-        }
-    json_object["name"]["male"] = translate_any(
-        translation, json_object["name"]["male"], context="scenario_male")
+    if "name" in json_object:
+        if type(json_object["name"]) is str:
+            scenario_name = json_object["name"]
+            json_object["name"] = {
+                "male": scenario_name, "female": scenario_name
+            }
+        json_object["name"]["male"] = translate_any(
+            translation, json_object["name"]["male"], context="scenario_male")
 
-    json_object["name"]["female"] = translate_any(
-        translation, json_object["name"]["female"], context="scenario_female")
+        json_object["name"]["female"] = translate_any(
+            translation, json_object["name"]["female"], context="scenario_female")
 
-    if type(json_object["description"]) is str:
-        scenario_description = json_object["description"]
-        json_object["description"] = {
-            "male": scenario_description, "female": scenario_description
-        }
-    json_object["description"]["male"] = translate_any(
-        translation, json_object["description"]["male"], context="scen_desc_male"
-    )
+    if "description" in json_object:
+        if type(json_object["description"]) is str:
+            scenario_description = json_object["description"]
+            json_object["description"] = {
+                "male": scenario_description, "female": scenario_description
+            }
+        json_object["description"]["male"] = translate_any(
+            translation, json_object["description"]["male"], context="scen_desc_male"
+        )
 
-    json_object["description"]["female"] = translate_any(
-        translation, json_object["description"]["female"], context="scen_desc_female"
-    )
+        json_object["description"]["female"] = translate_any(
+            translation, json_object["description"]["female"], context="scen_desc_female"
+        )
 
     if "start_name" in json_object:
         json_object["start_name"] = translate_any(
