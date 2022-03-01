8a38392978db23101c61be2cac47b90f06c89141
jiawei <37257891+HeYaoDaDa@users.noreply.github.com>
2022-03-01 08:40:01 +0800

    add Widget: "text" translate

--- string_translate/translaters/widget.py
+++ string_translate/translaters/widget.py
@@ -7,3 +7,7 @@ def translate_widget(translation: NullTranslations, json_object):
         json_object["label"] = translate_any(translation, json_object["label"])
     if "strings" in json_object:
         json_object["strings"] = translate_any(translation, json_object["strings"])
+    if "phrases" in json_object:
+        for phrase in json_object["phrases"]:
+            if "text" in phrase:
+                phrase["text"] = translate_any(translation, phrase["text"])
